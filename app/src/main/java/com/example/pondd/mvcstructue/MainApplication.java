f98968a2386da59fb58bab6e644c26fa6ac570e8
ountzza <pond1982a@gmail.com>
2014-12-25 11:06:03 +0700

    use singleton to get screensize

--- app/src/main/java/com/example/pondd/mvcstructue/MainApplication.java
+++ app/src/main/java/com/example/pondd/mvcstructue/MainApplication.java
@@ -2,6 +2,8 @@ package com.example.pondd.mvcstructue;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by Pondd on 12/25/14 AD.
  */
@@ -10,5 +12,6 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         // init stuff here
+        Contextor.getInstance().init(getApplicationContext());
     }
 }
